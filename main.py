3b0910f7d751deb6826cada9090e08ed92c4fef6
RASanchezB <rasanchez@unitec.edu>
2018-11-02 08:21:29 -0600

    se conecta con la base de datos

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
+import redis
 from PyQt4 import QtCore, QtGui, uic
 
 class Ui_MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("main.ui", self)
-
+	db = redis.StrictRedis(host="159.89.34.186",password="papitopiernaslargas69",db=0,port="6379")
+	self.label.setText(db.get("saludo"))
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     windo = Ui_MainWindow()
