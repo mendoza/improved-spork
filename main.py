1e22803652d14bef83798db4ebe0b21837862b4d
Mata <mata@localhost.localdomain>
2018-11-07 07:29:50 -0600

    probadno

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("main.ui", self)
 	db = redis.StrictRedis(host="159.89.34.186",password="papitopiernaslargas69",db=0,port="6379")
-	self.label.setText(db.get("saludo"))
+	self.label.setText(db.get("CasoClave"))
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = Ui_MainWindow()
