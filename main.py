30a72292531e7d4c773fa730a5cbd56c2c88e3a0
Mata <mata@localhost.localdomain>
2018-11-07 17:11:48 -0600

    probando el github

--- main.py
+++ main.py
@@ -2,12 +2,31 @@ import sys
 import redis
 from PyQt4 import QtCore, QtGui, uic
 
-class Ui_MainWindow(QtGui.QMainWindow):
-    def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        uic.loadUi("main.ui", self)
+class Ui_MainWindow(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(Ui_MainWindow, self).__init__(parent=parent)
+        #QtGui.QMainWindow.__init__(self)
+        # uic.loadUi("main.ui", self)
 	db = redis.StrictRedis(host="159.89.34.186",password="papitopiernaslargas69",db=0,port="6379")
 	self.label.setText(db.get("CasoClave"))
+        super(Ui_MainWindow, self).__init__()
+        self.setupUI(self)
+
+    def initUI(self):
+
+        self.resize(640, 480)
+        self.setWindowTitle('Probando')
+        self.center()
+
+        self.show()
+
+    def center(self):
+        frameGm = self.frameGeometry()
+        screen = QtGui.QApplication.desktop().screenNumber(QtGui.QApplication.desktop().cursor().pos())
+        centerPoint = QtGui.QApplication.desktop().screenGeometry(screen).center()
+        frameGm.moveCenter(centerPoint)
+        self.move(frameGm.topLeft())
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = Ui_MainWindow()
