e732efa1e21ff96c401c5ab2cb617587d3a64e10
David Mendoza <d.mendoza99@unitec.edu>
2018-11-09 12:14:47 -0600

    prueba

--- main.py
+++ main.py
@@ -2,26 +2,19 @@ import sys
 import redis
 from PyQt4 import QtCore, QtGui, uic
 
-<<<<<<< HEAD
 
-class Ui_MainWindow(QtGui.QMainWindow):
-=======
 class MainWindow(QtGui.QMainWindow):
->>>>>>> 27b1c64b0a574eb9e7c28e9051a594d6e5a06944
     def __init__(self):
         super(MainWindow, self).__init__()
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("main.ui", self)
-<<<<<<< HEAD
+        db = redis.StrictRedis(
+            host="159.89.34.186", password="papitopiernaslargas69", db=0, port="6379")
+        self.label.setText(db.get("CasoClave"))
 
 
-=======
-	db = redis.StrictRedis(host="159.89.34.186",password="papitopiernaslargas69",db=0,port="6379")
-	self.label.setText(db.get("CasoClave"))
->>>>>>> 27b1c64b0a574eb9e7c28e9051a594d6e5a06944
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
- 
