884eda8cb22aa455254e8446b5f755bc1c69484b
David Mendoza <d.mendoza99@unitec.edu>
2018-11-09 12:12:08 -0600

    idk

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
 
+
 class Ui_MainWindow(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         uic.loadUi("main.ui", self)
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
