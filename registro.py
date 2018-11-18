1c491799e805ea42ce8c860ca852f66ec8637d48
DiegoMatamoros <dam_900@unitec.edu>
2018-11-18 13:07:46 -0600

    arreglado farmacia.ui

--- registro.py
+++ registro.py
@@ -6,43 +6,63 @@ from functools import partial
     
 class ui_registro(QtGui.QDialog):
     def signin(self):
-        # if aqui para validar :v
-        length = self.db.llen("personas")
-        lista = []
-        for i in range(length):
-            lista.append(self.db.lindex("personas", i))
-        if self.id_edit.text() in lista:
-
-            msg = QtGui.QMessageBox()
-            msg.setIcon(QtGui.QMessageBox.Information)
-            msg.setText("ID esta en lista")
-            msg.setWindowTitle("ALERT")
-            msg.setStandardButtons(QtGui.QMessageBox.Ok)
-            msg.exec_()
-
-        else:
-            self.usuario["nombre"] = self.nombre_edit.text()
-            self.usuario["direccion"] = self.direccion_edit.text()
-            self.usuario["identidad"] = self.id_edit.text()
-            self.usuario["edad"] = self.edad_spinner.value()
-            self.usuario["password"] = self.password_edit.text()
-            if self.rb_masculino.isChecked():
-                self.usuario["sexo"] = "masculino"
-            elif self.rb_femenino.isChecked():
-                self.usuario["sexo"] = "femenino"
-            elif self.rb_otro.isChecked():
-                self.usuario["sexo"] = "otro"
+        maistra = self.db.get("passwordadmin")
+        if maistra == self.ContraMaistra_edit.text():
+            # if aqui para validar :v
+            length = self.db.llen("personas")
+            lista = []
+            for i in range(length):
+                lista.append(self.db.lindex("personas", i))
+            if self.id_edit.text() in lista:
+                msg = QtGui.QMessageBox()
+                msg.setIcon(QtGui.QMessageBox.Information)
+                msg.setText("ID esta en lista")
+                msg.setWindowTitle("ALERT")
+                msg.setStandardButtons(QtGui.QMessageBox.Ok)
+                msg.exec_()
             else:
-                self.usuario["sexo"] = "N/A"
-            self.db.hmset(self.usuario["identidad"], self.usuario)
-            self.db.lpush("personas", self.usuario["identidad"])
-
+                self.usuario["nombre"] = self.nombre_edit.text()
+                self.usuario["direccion"] = self.direccion_edit.text()
+                self.usuario["identidad"] = self.id_edit.text()
+                self.usuario["edad"] = self.edad_spinner.value()
+                self.usuario["password"] = self.password_edit.text()
+                if self.rb_masculino.isChecked():
+                    self.usuario["sexo"] = "masculino"
+                elif self.rb_femenino.isChecked():
+                    self.usuario["sexo"] = "femenino"
+                elif self.rb_otro.isChecked():
+                    self.usuario["sexo"] = "otro"
+                else:
+                    self.usuario["sexo"] = "N/A"
+                    self.usuario["es_jefe"] = True
+                if self.rb_farmacia.isChecked():
+                    self.usuario["departamento"] = "farmacia"
+                elif self.rb_laboratorio.isChecked():
+                    self.usuario["departamento"] = "laboratorio"
+                else:
+                    msg = QtGui.QMessageBox()
+                    msg.setIcon(QtGui.QMessageBox.Information)
+                    msg.setText("No se creo el usuario")
+                    msg.setWindowTitle("ALERT")
+                    msg.setStandardButtons(QtGui.QMessageBox.Ok)
+                    if msg.exec_():
+                        return 
+                self.db.hmset(self.usuario["identidad"], self.usuario)
+                self.db.lpush("personas", self.usuario["identidad"])
+                msg = QtGui.QMessageBox()
+                msg.setIcon(QtGui.QMessageBox.Information)
+                msg.setText("Funciono")
+                msg.setWindowTitle("ALERT")
+                msg.setStandardButtons(QtGui.QMessageBox.Ok)
+                msg.exec_()
+        else:
             msg = QtGui.QMessageBox()
             msg.setIcon(QtGui.QMessageBox.Information)
-            msg.setText("Funciono")
+            msg.setText("No se creo el usuario")
             msg.setWindowTitle("ALERT")
             msg.setStandardButtons(QtGui.QMessageBox.Ok)
             msg.exec_()
+
 
     def __init__(self):
         self.usuario = {
@@ -52,6 +72,8 @@ class ui_registro(QtGui.QDialog):
             "identidad": "",
             "sexo": "",
             "password": "",
+            "es_jefe":False,
+            "departamento":""
         }
         super(ui_registro, self).__init__()
         QtGui.QMainWindow.__init__(self)
@@ -67,9 +89,3 @@ if __name__ == "__main__":
     window = ui_registro()
     window.show()
     sys.exit(app.exec_())
-
-"""import redis
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-for key in r.scan_iter("user:*"):
-    # delete the key
-    r.delete(key)"""
