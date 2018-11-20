20596ba2cf49f10770ad784823d91052f898ccc9
Ricardo Sanchez <rasanchez@unitec.edu>
2018-11-20 01:19:42 -0600

    Mejorando el ui y empezando con la progra

--- main_lab.py
+++ main_lab.py
@@ -5,6 +5,27 @@ import redis
 from PyQt4 import QtCore, QtGui, uic
 
 class MainWindowLab(QtGui.QMainWindow):
+
+    def CrearP(self):
+        ID = 0 #Lo hare incapaz de cambio
+        Nombre = str(self.NombreP_edit.text())
+        Fabricante = str(self.FabricanteP_cb.text())
+        CostoVP = 0 #Cambiar a spinner
+        CosteP = 0 #Cambiar a spinner
+        UnidadP = int(self.Unidades_sp.value())
+        SeguridadP = 0
+        Familia = str(Familia_cb.text())
+
+    def ListarLabs(self):
+        
+    def ListarProducto(self):
+
+    def VentasR(self):
+
+    def Pedidos(self):
+
+    def ListaFamilias(self):
+
     def __init__(self):
         super(MainWindowLab, self).__init__()
         QtGui.QMainWindow.__init__(self)
@@ -13,9 +34,9 @@ class MainWindowLab(QtGui.QMainWindow):
             host="159.89.34.186", password="papitopiernaslargas69", db=0, port="6379"
         )
 
-
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     window = MainWindowLab()
     window.show()
     sys.exit(app.exec_())
+
